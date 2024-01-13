3fb3673d2c83822cebb83c9b0693cbcc94dc5ec1
ytejeda03 <y.tejeda03@gmail.com>
2024-01-13 07:23:16 -0500

    Implemented basic logic and render for ResetPasswordConfirmation page and fix error in login

---
+++ Core/OpenCRM.Core.Web/Areas/Identity/Pages/ResetPasswordConfirmation/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using OpenDHS.Shared.Data;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+
+namespace OpenCRM.Core.Web.Areas.Identity.Pages.ResetPasswordConfirmation
+{
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    [AllowAnonymous]
+    public class IndexModel : PageModel
+    {
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public void OnGet()
+        {
+        }
+    }
+}
