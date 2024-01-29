e53a654a401af86cb8d65fde74835aa7ae394ee7
Yariel Rodriguez <yariel.re@gmail.com>
2024-01-29 15:03:13 +0100

    Add CorePageModel with links bind property

--- Modules/OpenCRM.SwissLPD/Areas/SwissLDP/Pages/Index.cshtml.cs
+++ Modules/OpenCRM.SwissLPD/Areas/SwissLDP/Pages/Index.cshtml.cs
@@ -1,11 +1,8 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using OpenCRM.Core.Web.Models;
-using OpenCRM.Core.Web.Pages;
 
 namespace OpenCRM.SwissLPD.Areas.SwissLDP.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel : CorePageModel
     {
         public void OnGet()
         {
