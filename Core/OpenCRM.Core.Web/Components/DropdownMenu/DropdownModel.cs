f47f9538f5659559c8b84a481575fed4f4c3a6ae
Grettel Hernández Garbey <91501281+grettelHdez@users.noreply.github.com>
2024-01-30 11:31:21 -0500

    Dropdown Menu Modules update with the main modules and their submenu

---
+++ Core/OpenCRM.Core.Web/Components/DropdownMenu/DropdownModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCRM.Core.Web.Components.DropdownMenu
+{
+    public class DropdownModel
+    {
+        private string _name;
+
+        private string _url;
+
+        private List<DropdownModel> _items;
+
+
+        public DropdownModel(string name = "", string url = "")
+        {
+            _name = name;
+            _url = url;
+            _items = new List<DropdownModel>();
+        }
+
+        public DropdownModel(string name, string url, List<DropdownModel> items)
+        {
+            _name = name;
+            _url = url;
+            _items = items;
+        }
+
+        public string Name { get { return _name; } set { _name = value; } }
+
+        public string Url { get { return _url; } set { _url = value; } }
+
+        public List<DropdownModel> Items { get { return _items; } set { _items = value; } }
+
+    }
+}
---
+++ Core/OpenCRM.Core.Web/Components/Navbar.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using OpenCRM.Core.Web.Components.DropdownMenu;
+using OpenCRM.Core.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCRM.Core.Web.Components
+{
+    public partial class Navbar : ComponentBase
+    {
+        [Parameter]
+        public List<BreadCrumbLinkModel> Links { get; set; } = new List<BreadCrumbLinkModel>();
+
+        [Parameter]
+        public DropdownModel DropdownMenuModules { get; set; } = new DropdownModel()
+        {
+            Name = "Modules",
+            Items = new List<DropdownModel>()
+            {
+                new DropdownModel("Management", "/Manage"),
+
+                new DropdownModel("Register", "/Identity/Register"),
+
+                new DropdownModel("Login", "/Identity/Auth"),
+
+                new DropdownModel("SwissLDP", "/SwissLDP", new List<DropdownModel>()
+                {
+                    new DropdownModel("Event", "/SwissLDP/Event"),
+                    new DropdownModel("Supplier", "/SwissLDP/Supplier")
+                }),
+
+                new DropdownModel("Finance", "/Finance", new List<DropdownModel>()
+                {
+                    new DropdownModel("Accounting", "/Finance/Accounting")
+                }),
+            }
+        };
+    }
+}
