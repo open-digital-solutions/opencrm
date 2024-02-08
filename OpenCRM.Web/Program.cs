a61f3a503a7cf86df9c73c8bdc6a81248f12d5cb
Yariel Rodriguez <yariel.re@gmail.com>
2024-02-08 10:34:34 +0100

    Update https & launch settings

--- OpenCRM.Web/Program.cs
+++ OpenCRM.Web/Program.cs
@@ -11,15 +11,19 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseHttpsRedirection();
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
+//Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseHttpsRedirection();
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
 app.UseStaticFiles();
 
