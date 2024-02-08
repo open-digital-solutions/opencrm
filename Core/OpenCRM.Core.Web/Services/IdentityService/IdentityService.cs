79c2a28876f5601699e0aa494025e27c59fb9236
Yariel Rodriguez <yariel.re@gmail.com>
2024-02-08 16:04:14 +0100

    Update partiallya auth worlflow

--- Core/OpenCRM.Core.Web/Services/IdentityService/IdentityService.cs
+++ Core/OpenCRM.Core.Web/Services/IdentityService/IdentityService.cs
@@ -60,6 +60,10 @@ namespace OpenCRM.Core.Web.Services.IdentityService
             {
                 user.Email = Input.Email;
             }
+            if (Input.Email != "")
+            {
+                user.Email = Input.Email;
+            }
 
             var userCryptoData = RSACryptoService.GetKeyPairs();
             if (userCryptoData != null)
@@ -77,6 +81,7 @@ namespace OpenCRM.Core.Web.Services.IdentityService
         public async Task<bool> SendConfirmationEmail(UserEntity user, PageModel page)
         {
 
+
             var userId = await _userManager.GetUserIdAsync(user);
             var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
             var encodedCode = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
@@ -86,11 +91,6 @@ namespace OpenCRM.Core.Web.Services.IdentityService
             if (callbackUrl == null || string.IsNullOrEmpty(user.Email))
             {
                 return false;
-            }
-
-            var emailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
-            if (!emailConfirmed) {
-                await _userManager.ConfirmEmailAsync(user, encodedCode);
             }
 
             var emailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
