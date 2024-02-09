3e91301a7327d2d422b0d24819d4979b2c03352f
Grettel Hernández Garbey <91501281+grettelHdez@users.noreply.github.com>
2024-02-09 11:35:29 -0500

    Added IBlockModel, BlockTextModel, BlockCardModel

---
+++ Core/OpenCRM.Core.Web/Areas/Manage/Pages/Blocks/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OpenCRM.Core.Web.Areas.Manage.Pages.DataBlock
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Core/OpenCRM.Core.Web/Models/BlockModel/BlockTextModel.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCRM.Core.Web.Models.BlockModel
+{
+    public enum BlockType
+    {
+        Text,
+        Card
+    }
+
+    public class BlockDescription
+    {
+        public string? Text { get; set; }
+
+        public List<BlockDescription>? ItemsList { get; set; }
+    }
+
+    public class BlockTextModel : IBlockModel
+    {
+        public Guid Id { get; set; }
+
+        public BlockType Type { get; set; } = BlockType.Text;
+
+        [Required]
+        public string MainText { get; set; } = string.Empty;
+
+        public string? SubText { get; set; }
+
+        public BlockDescription? Description { get; set; }
+
+    }
+
+    public class BlockCardModel : IBlockModel
+    {
+		public Guid Id { get; set; }
+
+		public BlockType Type { get; set; } = BlockType.Card;
+
+		[Required]
+		public string MainText { get; set; } = string.Empty;
+
+		public string? SubText { get; set; }
+
+		public BlockDescription? Description { get; set; }
+
+		public string Image = string.Empty;
+    }
+}
