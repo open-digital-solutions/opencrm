3929fa9150f2a096bed0eda6d50e844221ff73d2
Grettel Hernández Garbey <91501281+grettelHdez@users.noreply.github.com>
2024-02-09 11:54:32 -0500

    Update BlockModel & Implement BlockService

---
+++ Core/OpenCRM.Core.Web/Models/BlockModel/BlockCardModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCRM.Core.Web.Models.BlockModel
+{
+	public class BlockCardModel : IBlockModel
+	{
+		public Guid Id { get; set; }
+
+		public BlockType Type { get; set; } = BlockType.Card;
+
+		[Required]
+		public string MainText { get; set; } = string.Empty;
+
+		public string? SubText { get; set; }
+
+		public BlockDescription? Description { get; set; }
+
+		public string Image = string.Empty;
+	}
+}
