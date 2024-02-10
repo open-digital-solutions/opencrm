88a3bcfabb866751fe65bb60acfe699f456a5181
Deneb Moliner Morales <dnbmorales@gmail.com>
2024-02-09 21:58:44 -0500

    fix min Create Media

--- Core/OpenCRM.Core.Web/Areas/Manage/Pages/Media/Create.cshtml.cs
+++ Core/OpenCRM.Core.Web/Areas/Manage/Pages/Media/Create.cshtml.cs
@@ -12,11 +12,39 @@ namespace OpenCRM.Core.Web.Areas.Manage.Pages.Media
         private readonly IMediaService _mediaService;
         [BindProperty]
         public MediaModel Model { get; set; } = default!;
+        [BindProperty]
+        public List<BreadCrumbLinkModel> Links { get; set; } = new List<BreadCrumbLinkModel>();
 
 
         public CreateModel(IMediaService mediaService)
         {
             _mediaService = mediaService;
+            Links.Add(new BreadCrumbLinkModel()
+            {
+                Area = "",
+                IsActive = true,
+                Name = "Home",
+                Page = "",
+                Url = "/"
+            });
+
+            Links.Add(new BreadCrumbLinkModel()
+            {
+                Area = "Manage",
+                IsActive = true,
+                Name = "Medias List",
+                Page = "Media",
+                Url = "/Manage"
+            });
+
+            Links.Add(new BreadCrumbLinkModel()
+            {
+                Area = "Manage",
+                IsActive = true,
+                Name = "Create Media",
+                Page = "Media",
+                Url = "/Manage/Media/Create"
+            });
         }
 
         public IActionResult OnGet()
