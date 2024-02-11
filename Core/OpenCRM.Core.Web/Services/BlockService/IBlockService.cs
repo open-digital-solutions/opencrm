654b8c207c962325b4196c9301843e228fe6f36d
Grettel Hernández Garbey <91501281+grettelHdez@users.noreply.github.com>
2024-02-11 15:36:17 -0500

    BlockModel, Block Component & CRUD

--- Core/OpenCRM.Core.Web/Services/BlockService/IBlockService.cs
+++ Core/OpenCRM.Core.Web/Services/BlockService/IBlockService.cs
@@ -35,13 +35,13 @@ namespace OpenCRM.Core.Web.Services.BlockService
 {
     public interface IBlockService
 	{
-		Task<DataBlockModel<IBlockModel>?> AddBlock(DataBlockModel<IBlockModel> model);
+		Task<DataBlockModel<BlockModel>?> AddBlock(DataBlockModel<BlockModel> model);
 
-		Task<DataBlockModel<IBlockModel>?> EditBlock(DataBlockModel<IBlockModel> model);
+		Task<DataBlockModel<BlockModel>?> EditBlock(DataBlockModel<BlockModel> model);
 
-		Task<DataBlockModel<IBlockModel>?> GetBlock(Guid Id);
+		Task<DataBlockModel<BlockModel>?> GetBlock(Guid Id);
 
-		Task<List<DataBlockModel<IBlockModel>>> GetBlocks();
+		Task<List<DataBlockModel<BlockModel>>> GetBlocks();
 		Task RemoveBlock(Guid Id);
 
 		Task Seed();
