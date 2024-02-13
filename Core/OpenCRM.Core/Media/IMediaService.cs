1ff9645fae07e8a4bb79b2d1ae31c979a594676d
Grettel Hernández Garbey <91501281+grettelHdez@users.noreply.github.com>
2024-02-13 08:42:24 -0500

    Added Code in BlockModel

---
+++ Core/OpenCRM.Core/Media/IMediaService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace OpenCRM.Core
+{
+    public interface IMediaService
+    {
+        public Task<MediaEntity> PostFileAsync(IFormFile fileData, bool isPublic = false);
+        public Task PostFileAsync(MediaModel model);
+        public Task PostMultiFileAsync(List<MediaUploadModel> fileData);
+        public Task<MediaEntity> DownloadFileById(Guid uuid);
+        public List<MediaEntity> GetMedias();
+        public MediaEntity GetMedia(Guid Id);
+        Task RemoveMedia(Guid Id);
+        public Task<MediaEntity> EditFileAsync(Guid Id,MediaEntity media);
+
+        public string GetImageUrl(Guid id);
+    }
+}
