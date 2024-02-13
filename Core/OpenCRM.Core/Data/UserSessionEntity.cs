3b2ff7667b4ba8d8353d504611f0c49a58bed5f4
Yariel Rodriguez <yariel.re@gmail.com>
2024-02-13 11:47:34 +0100

    Add UserSession Data entity

---
+++ Core/OpenCRM.Core/Data/UserSessionEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenCRM.Core.Data
+{
+    public class UserSessionEntity : BaseEntity
+    {
+        public required string UserId { get; set; }
+        public DateTime IssuedDate { get; set; }
+        public required string CypherToken { get; set; }
+    }
+}
