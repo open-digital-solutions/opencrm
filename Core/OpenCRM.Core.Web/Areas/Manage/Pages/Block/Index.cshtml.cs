da3cf2a5ef4634ddf275ff80f8db0f65e5fa99ff
Grettel Hernández Garbey <91501281+grettelHdez@users.noreply.github.com>
2024-02-13 12:38:25 -0500

    Fixing problems

--- Core/OpenCRM.Core.Web/Areas/Manage/Pages/Block/Index.cshtml.cs
+++ Core/OpenCRM.Core.Web/Areas/Manage/Pages/Block/Index.cshtml.cs
@@ -2,16 +2,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OpenCRM.Core.DataBlock;
-<<<<<<<< HEAD:Core/OpenCRM.Core.Web/Areas/Manage/Pages/Block/Index.cshtml.cs
 using OpenCRM.Core.Web.Components.Table;
 using OpenCRM.Core.Web.Models;
 using OpenCRM.Core.Web.Services.BlockService;
 using OpenCRM.Core.Web.Table;
-========
-using OpenCRM.Core.Web.Components.Block;
-using OpenCRM.Core.Web.Models;
-using OpenCRM.Core.Web.Services.BlockService;
->>>>>>>> b470273 (BlockModel, Block Component & CRUD):Core/OpenCRM.Core.Web/Areas/Manage/Pages/Block/Details.cshtml.cs
 
 namespace OpenCRM.Core.Web.Areas.Manage.Pages.DataBlock
 {
@@ -20,7 +14,6 @@ namespace OpenCRM.Core.Web.Areas.Manage.Pages.DataBlock
     {
         private readonly IBlockService _blockService;
 
-<<<<<<<< HEAD:Core/OpenCRM.Core.Web/Areas/Manage/Pages/Block/Index.cshtml.cs
         private readonly TableService<BlockModel> _tableService = new TableService<BlockModel>();
 
         [BindProperty]
@@ -32,14 +25,10 @@ namespace OpenCRM.Core.Web.Areas.Manage.Pages.DataBlock
         [BindProperty]
         public List<BreadCrumbLinkModel> Links { get; set; } = new List<BreadCrumbLinkModel>();
 
-<<<<<<<< HEAD:Core/OpenCRM.Core.Web/Areas/Manage/Pages/Block/Index.cshtml.cs
         [BindProperty]
         public TableModel Table { get; set; } = new TableModel("Blocks", "Block");
 
         public IndexModel(IBlockService blockService) 
-========
-        public DetailsModel(IBlockService blockService)
->>>>>>>> b470273 (BlockModel, Block Component & CRUD):Core/OpenCRM.Core.Web/Areas/Manage/Pages/Block/Details.cshtml.cs
         {
             _blockService = blockService;
 
@@ -65,17 +54,12 @@ namespace OpenCRM.Core.Web.Areas.Manage.Pages.DataBlock
             {
                 Area = "Manage",
                 IsActive = true,
-<<<<<<<< HEAD:Core/OpenCRM.Core.Web/Areas/Manage/Pages/Block/Index.cshtml.cs
-                Name = "Block",
-========
                 Name = "Blocks",
->>>>>>>> b470273 (BlockModel, Block Component & CRUD):Core/OpenCRM.Core.Web/Areas/Manage/Pages/Block/Details.cshtml.cs
                 Page = "",
                 Url = "/Manage/Block"
             });
         }
 
-<<<<<<<< HEAD:Core/OpenCRM.Core.Web/Areas/Manage/Pages/Block/Index.cshtml.cs
         public async Task OnGet()
         {
             var blocks = await _blockService.GetBlocks();
@@ -89,19 +73,5 @@ namespace OpenCRM.Core.Web.Areas.Manage.Pages.DataBlock
                 Table.Rows = result.Item2;
             }
 		}
-========
-        public async Task<IActionResult> OnGetAsync(Guid id)
-        {
-            var dataBlockModel = await _blockService.GetBlock(id);
-
-            if (dataBlockModel == null)
-            {
-                return NotFound();
-            }
-
-            Model = dataBlockModel;
-            return Page();
-        }
->>>>>>>> b470273 (BlockModel, Block Component & CRUD):Core/OpenCRM.Core.Web/Areas/Manage/Pages/Block/Details.cshtml.cs
     }
 }
