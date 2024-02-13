214ec5d925b0f986da1e701e3b53a5719f561cf4
Yariel Rodriguez <yariel.re@gmail.com>
2024-02-13 11:50:04 +0100

    Add User RSA Information & Update Registration WFlow

--- Core/OpenCRM.Core.Web/Services/IdentityService/IdentityService.cs
+++ Core/OpenCRM.Core.Web/Services/IdentityService/IdentityService.cs
@@ -70,10 +70,12 @@ namespace OpenCRM.Core.Web.Services.IdentityService
                 user.Email = Input.Email;
             }
 
-            ////Serialize Later the real Extra Properties!!!
-            //var extra = new { Extra1 = "Extra1", Extra2 = "Extra2" };
-            //var extraJson = Input.UserExtras == null ? JsonSerializer.Serialize(extra) : Input.UserExtras;
-            //user.UserExtras = extraJson;
+            var userCryptoData = RSACryptoService.GetKeyPairs();
+            if (userCryptoData != null)
+            {
+                user.RSAPublic = userCryptoData.GetStringPublicKey();
+                user.RSAPrivate = userCryptoData.GetStringPrivateKey();
+            }
 
             await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
 
--- Core/OpenCRM.Core/Data/UserEntity.cs
+++ Core/OpenCRM.Core/Data/UserEntity.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using OpenCRM.Core.Crypto;
 using System.ComponentModel.DataAnnotations.Schema;
