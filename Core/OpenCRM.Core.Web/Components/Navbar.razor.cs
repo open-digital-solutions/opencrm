451f26292b462f54758208cabaf031866bda19d8
Yariel Rodriguez <yariel.re@gmail.com>
2024-02-13 09:50:13 +0100

    Add Identity user on navbar user name info

--- Core/OpenCRM.Core.Web/Components/Navbar.razor.cs
+++ Core/OpenCRM.Core.Web/Components/Navbar.razor.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web;
 using OpenCRM.Core.Web.Components.DropdownMenu;
 using OpenCRM.Core.Web.Models;
 using OpenCRM.Core.Web.Services.IdentityService;
