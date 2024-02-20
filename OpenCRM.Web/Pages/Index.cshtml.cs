b676a8265c8402e16001e6c21aeceff06a148d29
Yariel Rodriguez <yariel.re@gmail.com>
2024-02-20 16:56:25 +0100

    Add DataSession management

--- OpenCRM.Web/Pages/Index.cshtml.cs
+++ OpenCRM.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OpenCRM.Core.Web.Models;
-using OpenCRM.Core.Web.Pages;
+using OpenCRM.Core.Web.Services.IdentityService;
 
 namespace OpenCRM.Web.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IIdentityService _identityService;
 
         [BindProperty]
         public List<BreadCrumbLinkModel> Links { get; set; } = new List<BreadCrumbLinkModel>();
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public string? Lang { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IIdentityService identityService)
         {
             _logger = logger;
+            _identityService = identityService;
             var link = new BreadCrumbLinkModel()
             {
                 Area = "",
@@ -28,7 +32,9 @@ namespace OpenCRM.Web.Pages
 
         public void OnGet()
         {
-
+            var dataSesison = _identityService.GetSession();
+            if (dataSesison == null) Lang = "IT";
+            Lang = dataSesison != null ? dataSesison.Lang : "Default dal browser";
         }
     }
 }
