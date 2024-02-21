9bb8b757595c1ba88e745b0f0c0c3818fc7fab38
Yariel Rodriguez <yariel.re@gmail.com>
2024-02-21 08:50:30 +0100

    Update UserSessionEntity

--- Core/OpenCRM.Core/Data/UserSessionEntity.cs
+++ Core/OpenCRM.Core/Data/UserSessionEntity.cs
@@ -2,8 +2,17 @@
 {
     public class UserSessionEntity : BaseEntity
     {
-        public required string UserId { get; set; }
         public DateTime IssuedDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        public required string UserName { get; set; }
         public required string CypherToken { get; set; }
+
+        public Guid UserId { get; set; }
+        public UserEntity User { get; set; } = null!;
+
+        public Guid LanguageId { get; set; }
+        public LanguageEntity Language { get; set; } = null!;
+
     }
 }
