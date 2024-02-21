924e44a4d9a4fab16ba8338755b937ba98cb70cb
Grettel Hernández Garbey <91501281+grettelHdez@users.noreply.github.com>
2024-02-21 07:26:41 -0500

    Added Block Component

--- OpenCRM.Web/Pages/Index.cshtml.cs
+++ OpenCRM.Web/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OpenCRM.Core.Web.Models;
 using OpenCRM.Core.Web.Pages;
+using OpenCRM.Core.Web.Services.BlockService;
 
 namespace OpenCRM.Web.Pages
 {
@@ -9,12 +10,19 @@ namespace OpenCRM.Web.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        private readonly IBlockService _blockService;
+
         [BindProperty]
         public List<BreadCrumbLinkModel> Links { get; set; } = new List<BreadCrumbLinkModel>();
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [BindProperty]
+        public BlockModel Block { get; set; } = new BlockModel();
+
+        public IndexModel(ILogger<IndexModel> logger, IBlockService blockService)
         {
             _logger = logger;
+            _blockService = blockService;
+
             var link = new BreadCrumbLinkModel()
             {
                 Area = "",
@@ -26,9 +34,29 @@ namespace OpenCRM.Web.Pages
             Links.Add(link);
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
+            string id = "24c5d1e0-dc43-4dee-8790-cbf6d495e7f1";
+            var dataBlockModel = await _blockService.GetBlock(Guid.Parse(id));
 
+            if (dataBlockModel == null)
+            {
+                return NotFound();
+            }
+
+            var blockModel = new BlockModel
+            {
+                Code = dataBlockModel.Data.Code,
+                Title = dataBlockModel.Data.Title,
+                SubTitle = dataBlockModel.Data.SubTitle,
+                Type = dataBlockModel.Data.Type,
+                Description = dataBlockModel.Data.Description,
+                ImageUrl = dataBlockModel.Data.ImageUrl,
+            };
+
+            Block = blockModel;
+
+            return Page();
         }
     }
 }
