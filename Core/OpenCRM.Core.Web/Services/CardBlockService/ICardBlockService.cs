f4967bcfddb1d9600715da796400ec05f50bc1c4
Yariel Rodriguez <yariel.re@gmail.com>
2024-02-23 17:20:22 +0100

    Update CardBlockService Interface

--- Core/OpenCRM.Core.Web/Services/CardBlockService/ICardBlockService.cs
+++ Core/OpenCRM.Core.Web/Services/CardBlockService/ICardBlockService.cs
@@ -7,10 +7,12 @@ namespace OpenCRM.Core.Web.Services.CardBlockService
     {
         Task<DataBlockModel<CardBlockModel>?> AddBlock(DataBlockModel<CardBlockModel> model);
         CardBlockModel CreateBlockModel(string code, string title, string? subTitle, string? description, string? imageId);
+        Task<CardBlockModel?> EditBlock(CardBlockModel model);
         Task<DataBlockModel<CardBlockModel>?> EditBlock(DataBlockModel<CardBlockModel> model);
         Task<DataBlockModel<CardBlockModel>?> GetBlock(Guid Id);
+        Task<DataBlockModel<CardBlockModel>?> GetBlockByCode(string code);
         Task<List<DataBlockModel<CardBlockModel>>> GetBlocks();
-        Task<DataBlockModel<CardBlockModel>?> ShowCardBlock();
         Task RemoveBlock(Guid Id);
+        Task<DataBlockModel<CardBlockModel>?> ShowCardBlock();
     }
 }
