1b997f087a9f63936f409a1c4d22cc8adac0f236
Hurshel DeSouza Noguera <136677084+HurshelDeSouza@users.noreply.github.com>
2024-03-13 02:06:01 -0400

    Add the translation keys for the QR code generator.

---
+++ OpenCRM.Web/Services/QrItalianTranslations.cs
@@ -0,0 +1,63 @@
+﻿using OpenCRM.Core.Web.Models;
+using OpenCRM.Core.Web.Services.TranslationService;
+
+namespace OpenCRM.Web.Services;
+
+public class QrItalianTranslations
+{
+	public QrItalianTranslations() { }
+
+	private List<TranslationByLanguage> qritalianTranslations = new()
+	{
+		new TranslationByLanguage()
+		{
+			ID = Guid.Empty,
+			Key = "KEY_QR_GENERATORE",
+			Translation = "KEY_QR_GENERATORE",
+			LanguageId = Guid.Empty,
+			LanguageCode = "IT"
+		},
+
+		new TranslationByLanguage()
+		{
+			ID = Guid.Empty,
+			Key = "KEY_QR_TESTO",
+			Translation = "KEY_QR_TESTO",
+			LanguageId = Guid.Empty,
+		},
+
+        new TranslationByLanguage()
+        {
+            ID = Guid.Empty,
+            Key = "KEY_QR_INSERISCI_IL_TESTO",
+            Translation = "KEY_QR_INSERISCI_IL_TESTO",
+            LanguageId = Guid.Empty,
+        },
+
+        new TranslationByLanguage()
+		{
+			ID = Guid.Empty,
+			Key = "KEY_QR_GENERARE_CODICE",
+			Translation = "KEY_QR_GENERARE_CODICE",
+			LanguageId = Guid.Empty,
+		},
+
+		new TranslationByLanguage()
+		{
+			ID = Guid.Empty,
+			Key = "KEY_QR_QR_CODICE",
+			Translation = "KEY_QR_QR_CODICE",
+			LanguageId = Guid.Empty,
+		},
+
+		new TranslationByLanguage()
+		{
+			ID = Guid.Empty,
+			Key = "KEY_QR_SCARICA_IL_CODICE_QR",
+			Translation = "KEY_QR_SCARICA_IL_CODICE_QR",
+			LanguageId = Guid.Empty,
+		},
+	};
+
+	public List<TranslationByLanguage> Translations { get { return qritalianTranslations; } }
+}
